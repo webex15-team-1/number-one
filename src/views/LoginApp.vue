ee51c3c7cae954d97d38017911a5ffea56d58487
shini-leaf <shiniyou137@gmail.com>
2022-08-29 00:00:21 +0900

    いろいろから回ったので、元にもどしました

--- src/views/LoginApp.vue
+++ src/views/LoginApp.vue
@@ -1,22 +1,65 @@
 <template>
   <h1>Let's Login</h1>
-  <MailLoginCom></MailLoginCom>
+  <table>
+    <tr>
+      <th>メールアドレス：</th>
+    </tr>
+    <tr>
+      <td><input type="email" v-model="email" /></td>
+    </tr>
+    <tr>
+      <th>パスワード：</th>
+    </tr>
+    <tr>
+      <td><input type="password" v-model="password" /></td>
+    </tr>
+  </table>
+  <button v-on:click="login">ログインする</button>
   <br />
-  <GoogleLoginCom></GoogleLoginCom>
+
+  <button v-on:click="googleLogin">googleでログイン</button>
+
+  <button @click="logout">ログアウト</button>
 </template>
 
 <script>
-import MailLoginCom from "@/components/MailLoginCom.vue"
-import GoogleLoginCom from "@/components/GoogleLoginCom.vue"
+import {
+  getAuth,
+  signOut,
+  signInWithEmailAndPassword,
+  signInWithRedirect,
+  GoogleAuthProvider,
+} from "firebase/auth"
 
 export default {
   data() {
-    return {}
+    return {
+      email: "",
+      password: "",
+    }
   },
-  methods: {},
-  components: {
-    MailLoginCom,
-    GoogleLoginCom,
+  methods: {
+    login() {
+      const auth = getAuth()
+      signInWithEmailAndPassword(auth, this.email, this.password)
+        .then(
+          //成功時の処理
+          alert("ログイン成功しました")
+        )
+        .catch(
+          //エラー時処理
+          alert("ログインできませんでした")
+        )
+    },
+    googleLogin() {
+      const auth = getAuth()
+      const provider = new GoogleAuthProvider()
+      signInWithRedirect(auth, provider)
+    },
+    logout() {
+      const auth = getAuth()
+      signOut(auth)
+    },
   },
 }
 </script>
