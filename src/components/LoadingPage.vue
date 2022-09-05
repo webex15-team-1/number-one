d84b0757656d806f14211b63d4860e89c2e5349f
junsakamaki <junmoco13@icloud.com>
2022-09-05 22:00:59 +0900

    loadingページを作りました。HomeView.vueを太陽のやつに変更しました。

---
+++ src/components/LoadingPage.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="loading">
+    <Icon class="loadingSun" icon="bi:sun-fill" color="white" width="10%" />
+  </div>
+</template>
+
+<script>
+import { Icon } from "@iconify/vue"
+export default {
+  components: {
+    Icon,
+  },
+}
+</script>
+
+<style>
+.loading {
+  background-color: #f2c48d;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.loadingSun {
+  animation: rotate 4s linear infinite;
+}
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
