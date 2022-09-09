650adef46af0187888628514a3803aed7198e784
SakaMax <sakaguchi.kosuke.83z@st.kyoto-u.ac.jp>
2022-09-09 20:40:37 +0900

    rankingをfirebaseの形式に沿って変更.

--- src/components/MypageRanking.vue
+++ src/components/MypageRanking.vue
@@ -2,33 +2,38 @@
   <div class="rank-container">
     <h2>ランキング</h2>
     <h3 v-for="(name, index) in ranking" :key="index">
-      {{ index }}位 {{ name }}
+      {{ index + 1 }}位 {{ name }}
     </h3>
   </div>
 </template>
 <script>
-import { collection, query, orderBy, limit, getDocs } from "firebase/firestore"
+import { collection, query, getDocs } from "firebase/firestore"
 import { db } from "@/firebase"
 export default {
   data() {
     return {
       ranking: [],
+      totalPoints: [],
     }
   },
   async mounted() {
-    const q = query(
-      collection(db, "users"),
-      orderBy("points", "desc"),
-      limit(3)
-    )
+    const q = query(collection(db, "users"))
     const querySnapshot = await getDocs(q)
     querySnapshot.forEach((doc) => {
-      this.ranking.push(doc.data().name)
+      const data = doc.data()
+      const getupPoints =
+        typeof data.getupPoints === "undefined" ? 0 : data.getupPoints
+      const timePoints =
+        typeof data.timePoints === "undefined" ? 0 : data.timePoints
+      this.totalPoints.push({
+        nickname: data.nickname,
+        points: getupPoints + timePoints,
+      })
     })
-    console.log(this.ranking)
-    // for (let i = 0; i < 3 - this.ranking.length; i++) {
-    //   this.ranking.push("")
-    // }
+    this.totalPoints.sort((a, b) => {
+      return b.points - a.points
+    })
+    this.ranking = this.totalPoints.map((value) => value.nickname).slice(0, 3)
   },
 }
 </script>
